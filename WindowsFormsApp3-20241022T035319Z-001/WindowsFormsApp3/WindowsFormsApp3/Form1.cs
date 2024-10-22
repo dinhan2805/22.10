6c3c3dea94b092fffc0576bc86988a19e3fe3243
dinhan2805 <binnguyen09112003@gmail.com>
2024-10-22 10:59:48 +0700

    Add files via upload

---
+++ WindowsFormsApp3-20241022T035319Z-001/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            if (username == "admin" && password == "1234")
+            {
+                MessageBox.Show("Dang nhap thanh cong","Message",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Dang nhap that bai","Message",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
